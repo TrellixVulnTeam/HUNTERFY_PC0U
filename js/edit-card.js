ecc4aff08384ce47dcfad6349892184c12a6b014
gvmolin <molinguin@gmail.com>
2021-08-16 03:04:20 -0300

    iniciando funcionamento dos cards

---
+++ js/edit-card.js
@@ -0,0 +1,9 @@
+function abrirCardEdit(){
+    var cardEdit = document.querySelector(".card-edit")
+    cardEdit.style.display = "block"
+}
+
+function fecharCardEdit(){
+    var cardEdit = document.querySelector(".card-edit")
+    cardEdit.style.display = "none"
+}
