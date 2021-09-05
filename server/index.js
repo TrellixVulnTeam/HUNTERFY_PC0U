883a0d5b9c68c9ba3805ad15ab033c315e63917f
gvmolinlinux <molinguin@gmail.com>
2021-09-05 19:18:34 -0300

    mais um teste

--- server/index.js
+++ server/index.js
@@ -14,6 +14,6 @@ dbClient.connect(error => {
         tables.criarTabela(dbClient)
     }
 })
-app.listen(process.env.PORT || 5432, ()=> console.log('servidor rodando na porta 3000'))
+app.listen(process.env.PORT || 5433, ()=> console.log('servidor rodando na porta 3000'))
 
 
