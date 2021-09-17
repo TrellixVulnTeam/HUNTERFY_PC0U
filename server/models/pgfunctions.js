c4b4ecb20373f1f3e07ef581d9d951380a55dd81
gvmolinlinux <molinguin@gmail.com>
2021-09-17 01:33:11 -0300

    att server

--- server/models/pgfunctions.js
+++ server/models/pgfunctions.js
@@ -134,7 +134,7 @@ class pgProgram{
 
         dbClient.query(insertQuery, (err, result)=>{
             if(!err){
-            console.log(result)
+            console.log(result.rows)
             //res.send(result.rows)
             }
             else{console.log(err.message)}
